2d234ab4d5c6ffb0cede8c3afc33f2f6a4a44b1a
root <root@ip-172-31-42-91.ec2.internal>
2025-04-06 14:19:47 +0000

    feat(authentication): add initial files for authentication service setup

---
+++ .linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/order-processing/authentication_component
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
